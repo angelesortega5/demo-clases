0d5287522e63e23197551eaa90f517f7842fb101
angelesortega <mariaortega5l@outlook.com>
2025-03-18 08:39:51 +0100

    se crea el proyecyo demo clases

---
+++ src/main/java/com/maol/demo/clases/dto/Animal.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.dto;
+
+public class Animal {
+
+	private String tipo = "perro";
+	private int numeroPatas = 4;
+	private double altura = 0.75;
+}
---
+++ src/main/java/com/maol/demo/clases/dto/Persona.java
@@ -0,0 +1,10 @@
+package com.maol.demo.clases.dto;
+
+public class Persona {
+
+	private String nombre = "maria";
+	private String apellido = "ortega";
+	private String genero = "femenina";
+	private int edad = 20;
+	private double altura = 1.80;
+}
