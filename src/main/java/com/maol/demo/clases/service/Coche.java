e850788b45705ab02ac8b7ba81d4815819928bb8
angelesortega <mariaortega5l@outlook.com>
2025-03-19 20:44:05 +0100

    guardando clases leon y coche

---
+++ src/main/java/com/maol/demo/clases/service/Coche.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Coche {
+
+	private String marca = "mazda";
+	private int añosGarantia = 30;
+	private double kilometraje = 65.5;
+}
---
+++ src/main/java/com/maol/demo/clases/service/Leon.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Leon {
+
+	private String tamanio = "mediano";
+	private int edad = 40;
+	private double altura = 2.55;
+}
