0a5251e058bc71ecfe30d5a7aa50a83728b92d1f
angelesortega <mariaortega5l@outlook.com>
2025-03-19 20:17:52 +0100

    guardando clases perro y gato en jave

---
+++ src/main/java/com/maol/demo/clases/service/Gato.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Gato {
+
+	private String color = "grisblanco";
+    private int edad = 6;
+    private double numeroPatas = 3.5;
+}
---
+++ src/main/java/com/maol/demo/clases/service/Perro.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Perro {
+
+	private String nombre = "tutti";
+	private int numeroCola =  1;
+	private double altura = 0.80;
+}
