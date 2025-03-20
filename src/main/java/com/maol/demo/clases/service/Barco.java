bea7f5e2172781a3efd3d1126fa29ad3ee1cc3dc
angelesortega <mariaortega5l@outlook.com>
2025-03-20 07:22:41 +0100

    guardando las clases barco y bicicleta

---
+++ src/main/java/com/maol/demo/clases/service/Barco.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Barco {
+
+	private String color = "negroblanco";
+	private int pesoKg = 3000;
+	private double medidaAncho = 95.5;
+}
---
+++ src/main/java/com/maol/demo/clases/service/Bicicleta.java
@@ -0,0 +1,8 @@
+package com.maol.demo.clases.service;
+
+public class Bicicleta {
+
+	private String tipo = "todoterreno";
+	private int pesoLb = 30;
+	private double numeroRin = 27.5;
+}
